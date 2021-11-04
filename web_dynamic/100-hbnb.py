98380041e0c3b3f77c43b6c52294af29f6b27aba
Tinsaye Simeneh <64516393+tinsaye-simeneh@users.noreply.github.com>
2021-11-04 14:34:51 +0300

    Update 100-hbnb.py

--- web_dynamic/100-hbnb.py
+++ web_dynamic/100-hbnb.py
@@ -1,48 +1,30 @@
 #!/usr/bin/python3
-"""
-Flask App that integrates with AirBnB static HTML Template
-"""
-from flask import Flask, render_template, url_for
+"""Flask app to generate complete html page containing location/amenity
+dropdown menus and rental listings"""
+from flask import Flask, render_template
 from models import storage
 import uuid
-
-# flask setup
-app = Flask(__name__)
+app = Flask('web_dynamic')
 app.url_map.strict_slashes = False
-port = 5000
-host = '0.0.0.0'
-
-
-# begin flask page rendering
-@app.teardown_appcontext
-def teardown_db(exception):
-    """
-    after each request, this method calls .close() (i.e. .remove()) on
-    the current SQLAlchemy Session
-    """
-    storage.close()
 
 
 @app.route('/100-hbnb')
-def hbnb_filters(the_id=None):
-    """
-    handles request to custom template with states, cities & amentities
-    """
-    state_objs = storage.all('State').values()
-    states = dict([state.name, state] for state in state_objs)
-    amens = storage.all('Amenity').values()
-    places = storage.all('Place').values()
-    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
-                 for user in storage.all('User').values())
+def display_hbnb():
+    """Generate page with popdown menu of states/cities"""
+    states = storage.all('State')
+    amenities = storage.all('Amenity')
     cache_id = uuid.uuid4()
     return render_template('100-hbnb.html',
                            states=states,
-                           amens=amens,
-                           places=places,
-                           users=users,
+                           amenities=amenities,
                            cache_id=cache_id)
 
-if __name__ == "__main__":
-    """
-    MAIN Flask App"""
-    app.run(host=host, port=port)
+
+@app.teardown_appcontext
+def teardown_db(*args, **kwargs):
+    """Close database or file storage"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
